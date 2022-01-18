301569ea6fbf500bdb877c16b7a1b44ad260d3d4
Didier Bonneau <didier.bonneau.vlb@gmail.com>
2022-01-18 11:42:21 +0100

    fin de la partie 4

--- src/Controller/VinController.php
+++ src/Controller/VinController.php
@@ -6,6 +6,8 @@ use App\Repository\VinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Vin;
+use Symfony\Component\HttpFoundation\Request;
 
 class VinController extends AbstractController
 {
@@ -18,4 +20,21 @@ class VinController extends AbstractController
             'vins' => $vins,
         ]);
     }
+
+    // #[Route('/vin/show/{id}', name: 'vin.show')]
+    // public function show(VinRepository $repo, Request $req): Response
+    // {
+    //     $id = $req->get('id');
+    //     $vin = $repo->findOneBy(['id' => $id]);
+    //     return $this->render('vin/show.html.twig', ['vin' => $vin]);
+
+    // }
+
+    #[Route('/vin/show/{id}', name: 'vin.show')]
+    public function show(Vin $vin): Response
+    {
+        return $this->render('vin/show.html.twig', ['vin' => $vin]);
+
+    }
+
 }
